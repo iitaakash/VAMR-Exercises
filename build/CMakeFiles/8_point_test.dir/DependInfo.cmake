011f09d0e4257d27a6f44eb128347a21e9304ce3
Aakash Murugan <iitaakash@gmail.com>
2020-06-17 22:25:38 -0400

    exercise 8 tracking complete

---
+++ build/CMakeFiles/8_point_test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/aakash/Desktop/VAMR-Exercises/exercise6/Exercise 6 - Two-view Geometry/triangulation/8_point_test.cpp" "/Users/aakash/Desktop/VAMR-Exercises/build/CMakeFiles/8_point_test.dir/8_point_test.cpp.o"
+  "/Users/aakash/Desktop/VAMR-Exercises/exercise6/Exercise 6 - Two-view Geometry/triangulation/Triangulate.cpp" "/Users/aakash/Desktop/VAMR-Exercises/build/CMakeFiles/8_point_test.dir/Triangulate.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv@3/3.4.10/include"
+  "/usr/local/Cellar/opencv@3/3.4.10/include/opencv"
+  "/usr/local/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/8_point_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "8_point_test"
+  "8_point_test.pdb"
+  "CMakeFiles/8_point_test.dir/8_point_test.cpp.o"
+  "CMakeFiles/8_point_test.dir/Triangulate.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_point_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
